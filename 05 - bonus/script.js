880d384213949e76bcbc94dfa74cda7fd08813ff
Simone-Fratini <simone@fratini.com>
2024-11-09 15:51:54 +0100

    esercizio 5

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -5,14 +5,21 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const name = 'Simone';
 
+function sayHi(name){
+    const date = new Date();
+    const hours = date.getHours();
+    let greeting = '';
+    if(hours < 13){
+        greeting = 'buongiorno';
+    } else if(hours < 17){
+        greeting = 'buon pomeriggio';
+    } else {
+        greeting = 'buonasera';
+    }
+    return `${greeting} ${name}`;
+}
 
-// Dichiara la funzione qui.
+console.log(sayHi(name));
 
-
-// Invoca la funzione qui e stampa il risultato in console
-
-
-
-//Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
