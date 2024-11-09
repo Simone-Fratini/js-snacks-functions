dc68c89f6d91b438e85fe155949e4925c7f5ff1d
Simone-Fratini <simone@fratini.com>
2024-11-09 15:49:47 +0100

    esercizio 3

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,10 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
+const name = 'Filippo';
 
 
-// Dichiara la funzione qui.
+function sayHi(name){
+    return `Ciao ${name}`;
+}
 
-
-// Invoca la funzione qui e stampa il risultato in console
-
-
-
-//Risultato atteso se si passa 'Mario': // ciao Mario
+console.log(sayHi(name));
